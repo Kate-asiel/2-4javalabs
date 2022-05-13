ab61966793fd08b3ab1d5d0c6db2a533c718b3cb
Kate-asiel <93286880+Kate-asiel@users.noreply.github.com>
2022-05-13 10:00:11 +0300

    Modifed Main

--- my/prject/Main.java
+++ my/prject/Main.java
@@ -2,5 +2,8 @@ package my.prject;
 
 public class Main {
     public static void main(String[] args) {
+        Regex regex = new Regex();
+        regex.deleteURL("You can follow the link http://www.xxxedu.com, and if you have problems with it, then use http://www.english.com or http://www.learnhacking.com");
     }
+    
 }
